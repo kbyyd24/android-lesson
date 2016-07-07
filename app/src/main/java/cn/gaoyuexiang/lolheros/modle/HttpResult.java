b5eb12f9fb2ab1040876347025016bb93da45f16
gaoyx <kbyyd24@outlook.com>
2016-07-07 11:31:17 +0800

    add HttpResult modle

---
+++ app/src/main/java/cn/gaoyuexiang/lolheros/modle/HttpResult.java
@@ -0,0 +1,33 @@
+package cn.gaoyuexiang.lolheros.modle;
+
+import java.util.List;
+
+/**
+ * Created by kbyyd on 2016/7/7.
+ */
+public class HttpResult {
+
+    private int errorCode;
+    private List<Hero> heros;
+
+    public HttpResult(int errorCode, List<Hero> heros) {
+        this.errorCode = errorCode;
+        this.heros = heros;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public List<Hero> getHeros() {
+        return heros;
+    }
+
+    public void setHeros(List<Hero> heros) {
+        this.heros = heros;
+    }
+}
