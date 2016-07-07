4f43e5fc6b3b1f564ba8184a276382e66bdbb7d7
gaoyx <kbyyd24@outlook.com>
2016-07-07 15:56:54 +0800

    set id to String

--- app/src/main/java/cn/gaoyuexiang/lolheros/modle/Hero.java
+++ app/src/main/java/cn/gaoyuexiang/lolheros/modle/Hero.java
@@ -1,19 +1,17 @@
 package cn.gaoyuexiang.lolheros.modle;
 
-import java.util.List;
-
 /**
  * Created by kbyyd on 2016/7/7.
  */
 public class Hero {
 
-    private int id;
+    private String id;
     private String nameC;
     private String title;
     private String img;
     private String tags;
 
-    public Hero(int id, String nameC, String title, String img, String tags) {
+    public Hero(String id, String nameC, String title, String img, String tags) {
         this.id = id;
         this.nameC = nameC;
         this.title = title;
@@ -22,14 +20,14 @@ public class Hero {
     }
 
     public Hero() {
-        
+
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
