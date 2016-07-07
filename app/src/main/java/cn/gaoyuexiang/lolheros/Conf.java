d7fb69dcde98bc2ea94b7fc3277f8bed881dd566
gaoyx <kbyyd24@outlook.com>
2016-07-07 11:50:19 +0800

    add a conf class to save something

---
+++ app/src/main/java/cn/gaoyuexiang/lolheros/Conf.java
@@ -0,0 +1,8 @@
+package cn.gaoyuexiang.lolheros;
+
+/**
+ * Created by kbyyd on 2016/7/7.
+ */
+public class Conf {
+    public static final String API = "http://lol.data.shiwan.com/lolHeros/?filter=&type=free";
+}
