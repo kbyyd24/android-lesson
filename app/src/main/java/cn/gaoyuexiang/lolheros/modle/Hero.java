0bfc5c7d42f0a6ee0703d565e1309cc95a6498de
gaoyx <kbyyd24@outlook.com>
2016-07-07 11:29:21 +0800

    add Hero modle

---
+++ app/src/main/java/cn/gaoyuexiang/lolheros/modle/Hero.java
@@ -0,0 +1,63 @@
+package cn.gaoyuexiang.lolheros.modle;
+
+import java.util.List;
+
+/**
+ * Created by kbyyd on 2016/7/7.
+ */
+public class Hero {
+
+    private int id;
+    private String nameC;
+    private String title;
+    private String img;
+    private List<String> tags;
+
+    public Hero(int id, String nameC, String title, String img, List<String> tags) {
+        this.id = id;
+        this.nameC = nameC;
+        this.title = title;
+        this.img = img;
+        this.tags = tags;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameC() {
+        return nameC;
+    }
+
+    public void setNameC(String nameC) {
+        this.nameC = nameC;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+}
